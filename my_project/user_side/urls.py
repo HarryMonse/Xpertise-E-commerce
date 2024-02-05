26263d255d9cbf30495587ddf367eb43d00eb8a6
Harry Monse <harry.monse1812@gmail.com>
2024-02-05 19:23:38 +0530

    unedited foundation

--- my_project/user_side/urls.py
+++ my_project/user_side/urls.py
@@ -4,7 +4,6 @@ from user_side import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("/user_home", views.user_home, name="user_home"),
     path("/user_login", views.user_login, name="user_login"),
 
 
--- my_project/user_side/views.py
+++ my_project/user_side/views.py
@@ -5,9 +5,6 @@ from django.shortcuts import render
 def index(request):
     return render(request, "user_side/index.html")
 
-def user_home(request):
-    return render(request, "user_side/user_home.html")
-
 def user_login(request):
     return render(request, "user_side/user_login.html")
 
