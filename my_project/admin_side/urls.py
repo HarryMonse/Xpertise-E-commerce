24123d4ee5015b223714f5924ef0c1e34208d701
Harry Monse <harry.monse1812@gmail.com>
2024-02-13 15:58:55 +0530

    working on admin index

--- my_project/admin_side/urls.py
+++ my_project/admin_side/urls.py
@@ -4,4 +4,6 @@ from admin_side import views
 
 urlpatterns = [
     path('admin_login', views.admin_login , name ='admin_login'),
+    path('admin_index/',views.admin_index,name='admin_index'),
+
 ]
--- my_project/admin_side/views.py
+++ my_project/admin_side/views.py
@@ -1,6 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib import messages,auth
+from django.contrib.auth import authenticate, login,logout
+from user_side.models import *
+
+
 
 # Create your views here.
 
 def admin_login(request):
-    return render(request, "admin_side/admin_login.html")
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+        user=authenticate(request, email=email,password=password)
+        if user is not None and user.is_active and user.is_superadmin and user.is_staff and user.is_admin :
+            login(request, user)
+            messages.success(request, 'Successfully logged in.')
+            return redirect('admin_index')
+        else:
+            messages.error(request, 'Invalid credentials')
+            return render(request, 'admin_side/admin_login.html')
+    return render(request, 'admin_side/admin_login.html')
+
+
+
+def admin_index(request):
+    
+    return render(request, 'admin_side/admin_index.html')
