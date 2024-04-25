99fcca78e515cc77ac5b31ce4442e91f60735c96
Harry Monse <harry.monse1812@gmail.com>
2024-04-25 17:32:22 +0530

    registered few models

--- my_project/payment/admin.py
+++ my_project/payment/admin.py
@@ -6,3 +6,8 @@ from .models import *
 
 admin.site.register(Address)
 admin.site.register(CartItem)
+admin.site.register(Wallet)
+admin.site.register(WalletHistory)
+admin.site.register(Payments)
+admin.site.register(CartOrder)
+admin.site.register(ServiceOrder)
